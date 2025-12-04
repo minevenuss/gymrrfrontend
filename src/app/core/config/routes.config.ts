74b4c3930e81e042af0bc039b9d77a55e54a3563
Saneldoes <geivwcwjhfbwjof@gmail.com>
2025-12-04 00:38:52 -0400

    Navbar mas o menos lista.

--- src/app/core/config/routes.config.ts
+++ src/app/core/config/routes.config.ts
@@ -6,6 +6,11 @@ export const APP_ROUTES: Routes = [
     component: HomePage, title: 'Home Page'
   },
   {
+    path: 'landing',
+    loadChildren: () =>
+        import('../../features/landing/landing.routes').then(m=> m.LANDING_ROUTES)
+  },
+  {
     path: 'auth',
     loadChildren: () =>
         import('../../features/auth/auth.routes').then(m=> m.AUTH_ROUTES)
